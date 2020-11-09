6a5e15ac444833a8a0711a66a28ad1b65a3e1eda
Claudio Campos <executive.taxi95@gmail.com>
2020-11-09 08:46:38 -0300

    atualizando imagens e icones

---
+++ assets/js/style.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.scrolling a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        var id = $(this).attr('href'),
+            targetOffset = $(id).offset().top;
+
+        $('html, body').animate({
+            scrollTop: targetOffset - 100
+        }, 500);
+    });
+});
